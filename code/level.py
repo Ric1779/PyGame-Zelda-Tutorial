2bb23ccdba2bdc1f30bdcebfce0ed21ce5336b82
Richards <richards.1779@gmail.com>
2023-10-22 22:50:49 +0200

    particle-effect-2

--- code/level.py
+++ code/level.py
@@ -9,6 +9,7 @@ from weapon import Weapon
 from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
+from magic import MagicPlayer
 
 class Level:
     def __init__(self) -> None:
@@ -33,6 +34,7 @@ class Level:
 
         # particles
         self.animation_player = AnimationPlayer()
+        self.magic_player = MagicPlayer(self.animation_player)
 
     def create_map(self):
         layouts = {'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
@@ -77,7 +79,8 @@ class Level:
                                       (x,y), 
                                       [self.visible_sprites, self.attackable_sprites], 
                                       self.obstacle_sprites,
-                                      self.damage_player)
+                                      self.damage_player,
+                                      self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites]) 
@@ -88,9 +91,12 @@ class Level:
         self.current_attack = None
 
     def create_magic(self, style, strength, cost):
-        print(style)
-        print(strength)
-        print(cost)
+        
+        if style=="heal":
+            self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
+
+        if style=="flame":
+            pass
 
     def player_attack_logic(self):
         if self.attack_sprites:
@@ -114,7 +120,10 @@ class Level:
             self.player.hurt_time = pygame.time.get_ticks()
             
             # spawn particles
-            
+            self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
+
+    def trigger_death_particles(self, pos, particle_type):
+        self.animation_player.create_particles(particle_type, pos, [self.visible_sprites])
 
     def run(self):
         # update and draw the game
